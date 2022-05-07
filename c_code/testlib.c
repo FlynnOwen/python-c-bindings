99b3ed15a4084da70568ad2352f7de63697c8180
Flynn Owen <flynnoo49@gmail.com>
2022-05-07 13:23:06 +1200

    add function taking arguments

--- c_code/testlib.c
+++ c_code/testlib.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 void myprint(void);
+void print_num(int);
 
 void myprint()
 {
     printf("hello world\n");
+}
+
+void print_num(int num)
+{
+    printf("The number %d was entered%n", num);
 }
--- python_code/testlibwrapper.py
+++ python_code/testlibwrapper.py
@@ -2,3 +2,4 @@ import ctypes
 
 testlib = ctypes.CDLL('../c_code/executables/testlib.so')
 testlib.myprint()
+testlib.print_num(5)
